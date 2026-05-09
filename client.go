c17343a247d6e3a3045f4bd6d9bbcc08a9f1f51e
Moeweb647252 <102890864+Moeweb647252@users.noreply.github.com>
2026-05-09 11:21:09 +0800

    fix: fix syntax error

--- client.go
+++ client.go
@@ -118,8 +118,8 @@ func (d *Dialer) Dial(ctx context.Context, urlStr string, reqHdr http.Header) (*
 
 	tr := &http3.Transport{EnableDatagrams: true,
 		AdditionalSettings: map[uint64]uint64{
-			settingsEnableWebtransport: 1,
-		}
+			settingsWebTransportEnabled: 1,
+		},
 	}
 	rsp, sess, err := d.handleConn(ctx, tr, qconn, req)
 	if err != nil {
